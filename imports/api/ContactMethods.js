4aaa1b892af56176c74b1961c1c57964dc24dc1b
bumbuna <developer@devbumbuna.com>
2023-02-24 12:00:41 +0300

    added optimistic UI && using anon functions
    replaced lambda functions with anonymous ones in ContactMethods.js for proper 'this' binding

--- imports/api/ContactMethods.js
+++ imports/api/ContactMethods.js
@@ -1,20 +1,20 @@
-import {Meteor} from "meteor/meteor";
-import {ContactsCollection} from "./Contact";
+import { Meteor } from "meteor/meteor";
+import { ContactsCollection } from "./Contact";
 
 Meteor.methods({
-    'contacts.insert': ({name, email, profileImageUrl}) => {
-        if(!name || !email || !profileImageUrl) {
-            throw new Meteor.Error( (!name ? 'Name' : !email ? 'Email Address' : 'Profile image Url') + ' is required')
+    'contacts.insert': function ({ name, email, profileImageUrl }) {
+        if (!name || !email || !profileImageUrl) {
+            throw new Meteor.Error((!name ? 'Name' : !email ? 'Email Address' : 'Profile image Url') + ' is required')
         }
         return ContactsCollection.insert({
             name, email, profileImageUrl,
             addedOn: new Date()
         })
     },
-    'contacts.remove' : ({id}) => {
-        if(!id) {
+    'contacts.remove': function ({ id }) {
+        if (!id) {
             throw new Meteor.Error('please provide a valid Id')
         }
-        return ContactsCollection.remove({_id: id})
+        return ContactsCollection.remove({ _id: id })
     }
 })
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,32 +2,32 @@ import React from 'react';
 import AddContactForm from "./AddContactForm";
 import ContactList from "./ContactList";
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {Col, Row, Container, Navbar} from "react-bootstrap";
-import {Wallet} from "react-bootstrap-icons";
+import { Col, Row, Container, Navbar } from "react-bootstrap";
+import { Wallet } from "react-bootstrap-icons";
 
 export const App = () => (
     <>
-    <Navbar style={{
-        height:'80px',
-        marginBottom: '2px'
-    }} sticky={'top'} bg={'primary'}>
-<Container className={'fs-1 text-light d-flex'}>
-    <div>
-        <Wallet className={'fw-bolder'}/>
-        <span className={'fs-4 ps-3 fw-bolder'}>Bumbuna's Meteor Wallet</span>
-    </div>
-</Container>
-    </Navbar>
-<Container className={'pt-3'}>
-      <Row cols={2}>
-          <Col xs={12}>
-              <AddContactForm/>
-          </Col>
-          <Col>
-              <ContactList/>
-          </Col>
-      </Row>
+        <Navbar style={{
+            height: '80px',
+            marginBottom: '2px'
+        }} sticky={'top'} bg={'primary'}>
+            <Container className={'fs-1 text-light d-flex'}>
+                <div>
+                    <Wallet className={'fw-bolder'} />
+                    <span className={'fs-4 ps-3 fw-bolder'}>Bumbuna's Meteor Wallet</span>
+                </div>
+            </Container>
+        </Navbar>
+        <Container className={'pt-3'}>
+            <Row cols={2}>
+                <Col xs={12}>
+                    <AddContactForm />
+                </Col>
+                <Col>
+                    <ContactList />
+                </Col>
+            </Row>
 
-  </Container>
-        </>
+        </Container>
+    </>
 );
